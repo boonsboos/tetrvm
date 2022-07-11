2caef781de848a61d29960b194e60e774a824469
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2022-07-11 19:13:15 +0200

    encode in little endian instead

--- ops/octalconv.v
+++ ops/octalconv.v
@@ -15,14 +15,14 @@ const (
 // converts a number from split up octal to an int
 [direct_array_access]
 pub fn u8_arr_to_int(arr []u8) int {
-	return  arr[7] * pow_0 +
-			arr[6] * pow_1 +
-			arr[5] * pow_2 +
-			arr[4] * pow_3 +
-			arr[3] * pow_4 +
-			arr[2] * pow_5 +
-			arr[1] * pow_6 +
-			arr[0] * pow_7
+	return  arr[0] * pow_0 +
+			arr[1] * pow_1 +
+			arr[2] * pow_2 +
+			arr[3] * pow_3 +
+			arr[4] * pow_4 +
+			arr[5] * pow_5 +
+			arr[6] * pow_6 +
+			arr[7] * pow_7
 }
 
 // converts a number from int to split up octal
@@ -34,7 +34,7 @@ pub fn int_to_u8_arr(n int) []u8 {
 	for i, _ in arr {
 		di := nr / 8
 		mo := nr % 8
-		arr[i] = u8(mo)
+		arr[7-i] = u8(mo)
 		nr = di
 	}
 
