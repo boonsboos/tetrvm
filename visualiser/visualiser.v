d175310616495b18c57b2511e46019f2387c5dd8
MrsHerobrine (Naomi) <mrsherobrinenaomi@gmail.com>
2022-07-11 20:12:30 +0200

    visualiser prints readable blocks now

--- visualiser/visualiser.v
+++ visualiser/visualiser.v
@@ -20,14 +20,14 @@ pub fn visualise(filename string) {
 		}
 
 		match b {
-			0o00 { buf.write_string(term.reset(' ')) }
-			0o01 { buf.write_string(term.bright_bg_blue(' ')) }
-			0o02 { buf.write_string(term.bg_yellow(' ')) }
-			0o03 { buf.write_string(term.bg_magenta(' ')) }
-			0o04 { buf.write_string(term.bg_blue(' ')) }
-			0o05 { buf.write_string(term.bg_rgb(201, 107, 12, ' ')) }
-			0o06 { buf.write_string(term.bg_green(' ')) }
-			0o07 { buf.write_string(term.bright_bg_red(' ')) }
+			0o00 { buf.write_string('  ') }
+			0o01 { buf.write_string(term.bright_bg_blue('  ')) }
+			0o02 { buf.write_string(term.bg_yellow('  ')) }
+			0o03 { buf.write_string(term.bg_magenta('  ')) }
+			0o04 { buf.write_string(term.bg_blue('  ')) }
+			0o05 { buf.write_string(term.bg_rgb(201, 107, 12, '  ')) }
+			0o06 { buf.write_string(term.bg_green('  ')) }
+			0o07 { buf.write_string(term.bright_bg_red('  ')) }
 			else {
 				eprintln('your bytecode has an unsupported value. please check if it compiled correctly.')
 				exit(1)
